34c5bb97abf56e26bda40f943079a65779177ceb
0xearl <earljohnsabalo@gmail.com>
2021-03-02 15:35:33 +0800

    Adding Examples For Documentation

---
+++ Examples/basic.php
@@ -0,0 +1,20 @@
+<?php 
+require_once('../autoload.php');                                    // load your autoloader.php
+
+use Earl\Core\Uploader;
+
+$uploader = new Uploader('imageUpload', '/');                       // Set the filename and the destination of uploaded files
+
+/**
+ * You can use processImage() if your file being uploaded is an 
+ * image Or you can use processFile() if your file being 
+ * uploaded is an document/text/pdf etc
+ */
+$uploader->processImage();
+
+/**
+ * you can get the uploaded filename with the function getFileName();
+ * 
+ * you can now store $filename in your database.
+ */
+$filename = $uploader->getFileName();
---
+++ Examples/customize.php
@@ -0,0 +1,33 @@
+<?php 
+
+require_once('../autoload.php');                                                    // Load your autoload.php
+
+use Earl\Core\Uploader;                                                             // Use the uploader
+
+$uploader = new Uploader('testFile', '/');                                          // Set the filename and the destination of uploaded files
+
+/**
+ * customizing the valid file extensions and mime types
+ */
+$uploader->validFileExtensions = ['docx', 'doc', 'pdf'];                            // We are setting the valid extensions to only docx, doc and pdf.
+
+/**
+ * If you're going to customize the valid extensions please
+ * Customize the mimetypes according to the valid mime types
+ * Of your custom Exensions
+ * 
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
+ */
+$uploader->validFileMimeTypes = [
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'application/msword',                                                           
+    'application/pdf'                                                              
+];
+
+/**
+ * you can also do the same with image.
+ */
+
+ $uploader->validImageExtensions = ['png', 'jpeg'];
+
+ $uploader->validImageMimeTypes = ['image/png', 'image/jpeg'];
